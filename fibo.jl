06bed9ba567555c6ddb745aca436f2b1ca79431b
vaskodin <arivederchi9@gmail.com>
2021-05-13 13:53:48 +0300

    as

--- fibo.jl
+++ fibo.jl
@@ -19,38 +19,38 @@ end
 
 
 function fibo(n)
-    if n == 0
-        return 0
-    elseif n == 1 || n == 2
-        return 1
-    end
-    fibo(n-1) + fibo(n-2)
+    arr = [1, 1]
+    for i = 3:n
+       push!(arr, arr[i-1] + arr[i-2]) 
+   end
+   arr
 end
 n = 22
-fiboarr = [fibo(i) for i = 2:n]
+fiboarr = fibo(n)
 
 
+arrs = Float64[]
 arr = Float64[]
 par = Int64[]
 
-#=
-for i = 3:n
+for i = 2:n
     push!(par, fiboarr[i-1])
     push!(arr, f(fiboarr[i-1], fiboarr[i]))
 end
-ab = DataFrame(dots=fiboarr[3:n],
+#=ab = DataFrame(dots=fiboarr[3:n],
                a = par,
                data = arr)
 CSV.write("17711.csv", ab) 
 =#
 
 s = SobolSeq(2)
-
+popfirst!(fiboarr)
 for j in fiboarr
     p = reduce(hcat, next!(s) for i = 1:j)'
-    push!(arr, f(j, p[:,1], p[:,2]))
+    push!(arrs, f(j, p[:,1], p[:,2]))
 end
 ab = DataFrame(dots=fiboarr,
-               data = arr)
+               data1 = arr,
+              data2 = arrs)
 CSV.write("17711s.csv", ab) 
 
